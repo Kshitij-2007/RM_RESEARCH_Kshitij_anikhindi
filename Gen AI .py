d9c6d96dce918cf4fcd487cd010d9de9460ddfdc
kshitij <kshitijanikhindi@gmail.com>
2025-12-29 13:25:37 +0530

    adding edge ai folder

--- Gen AI .py
+++
@@ -1,5 +0,0 @@
-from openai import OpenAI
-client = OpenAI(api_key="API KEY")
-user_input = input("You: ")
-response = client.chat.completions.create(model="gpt-4o-mini",messages=[{"role": "user", "content": user_input}])
-print("LLM:", response.choices[0].message.content)
